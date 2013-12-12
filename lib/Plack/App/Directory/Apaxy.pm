d6d0ad610c85ae214aa225568659d52fba16f93b
Keedi Kim <keedi.k@gmail.com>
2013-12-13 05:33:11 +0900

    Add skeleton code for main module

---
+++ lib/Plack/App/Directory/Apaxy.pm
@@ -0,0 +1,13 @@
+package Plack::App::Directory::Apaxy;
+# ABSTRACT: Serve static files from document root with directory index using Apaxy
+
+1;
+
+=head1 SYNOPSIS
+
+    ...
+
+
+=head1 DESCRIPTION
+
+...
